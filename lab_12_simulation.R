710840a9152a44f55338ef1433970426a0e2a9ff
Kaushik Murali <kaushikm@andrew.cmu.edu>
2018-04-17 19:45:57 -0400

    2b commit

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,7 +1,20 @@
+## 2a)
+
 generate_data = function(n,p) {
   res = list()
   res[[1]] = matrix(rnorm(n*p, 0, 1), nrow = n, ncol = p)
   res[[2]] = rnorm(n, 0, 1)
-  names(res) = c("covariates", responses)
+  names(res) = c("covariates", "responses")
   return (res)
 }
+
+## 2b)
+
+model_select = function (covariates, responses, cutoff) {
+  
+  temp.lm = lm(responses ~ covariates)
+  index = which((summary(temp.lm)$coefficients)[,4] < 0.05) 
+  return((summary(lm(responses~covariates[, index]))$coefficients)[,4])
+}
+
+
