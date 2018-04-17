11200c1824401b1ab82834c87ac069f1ef634ab8
Kaushik Murali <kaushikm@andrew.cmu.edu>
2018-04-17 19:57:24 -0400

    2c commit

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -13,8 +13,24 @@ generate_data = function(n,p) {
 model_select = function (covariates, responses, cutoff) {
   
   temp.lm = lm(responses ~ covariates)
-  index = which((summary(temp.lm)$coefficients)[,4] < 0.05) 
+  index = which((summary(temp.lm)$coefficients)[,4] < cutoff) 
   return((summary(lm(responses~covariates[, index]))$coefficients)[,4])
 }
 
+## 2c)
 
+run_simulation = function (n_trials, n, p, cutoff) {
+  res = c()
+  for (i in 1:n_trials) {
+    dat = generate_data(n[i], p[i])
+    responses = dat$responses
+    covariates = dat$covariates
+    Ps = model_select(covariates, responses, cutoff)
+    res = c(res, Ps)
+    
+  }
+  return(hist(res, main = "Distribution of P-values"), xlab = "P-values")
+  
+} 
+
+run_simulation(3,c(100, 1000, 10000), c(10, 20, 50), 0.05)
